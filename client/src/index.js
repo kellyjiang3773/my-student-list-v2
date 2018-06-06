faa7d0044cce1ebae96741562a27ed6d3c48542f
Kelly Jiang <M35046@mcap.com>
2018-06-06 10:50:01 -0400

    ready to make tests

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import StudentListBox from './StudentListBox';
-import SideBarExample from './SideBar';
+import SideBarExample from './components/SideBar';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<SideBarExample />, document.getElementById('root'));
